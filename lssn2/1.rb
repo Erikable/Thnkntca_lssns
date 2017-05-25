4e962d5114585a75b3701620ae687177feb48996
Erikable <surmiloev@gmail.com>
2017-05-25 12:44:50 +0300

    first app_done

--- lssn2/1.rb
+++ lssn2/1.rb
@@ -1,1 +1,11 @@
-# app 1
+m = {'January' => 31, 'February' => 28,
+	'March' => 31, 'April' => 30, 'May' => 31,
+	'June' => 30, 'July' => 31, 'August' => 31, 
+	'September' => 30, 'October' => 31, 
+	'November' => 30, 'December' => 31}
+
+m.each do |z,x|
+	if x == 30
+	puts "there are #{x} in #{z}"
+end
+end
