70bc8a81e937cc454db6e9c70af5bfef50f2744d
Erikable <surmiloev@gmail.com>
2017-05-27 22:22:49 +0300

    1234done

--- lssn2/4.rb
+++ lssn2/4.rb
@@ -1,5 +1,13 @@
 # 4. Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
-x = ('а'..'я').to_a
-arr = []
-arr << x
-puts arr
+alf = ('a'..'z').to_a
+extract = %w[a e i o u y]
+num = {}
+extract.each do |q|
+	if alf.include?(q)
+		num[q] = alf.index(q) + 1
+	end
+end
+num.each do |key, value|
+	puts "#{key} - #{value}"
+end
+
