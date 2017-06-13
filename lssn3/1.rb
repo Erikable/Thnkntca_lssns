96d88f3533ec3da1fa47c28d2488068bc293638e
Erikable <surmiloev@gmail.com>
2017-06-13 22:26:39 +0300

    продолжаем..

--- lssn3/1.rb
+++ lssn3/1.rb
@@ -1,141 +1,159 @@
 =begin
-Класс Station (Станция):
-			-Имеет название, которое указывается при ее создании
-			-Может принимать поезда (по одному за раз)
-			-Может возвращать список всех поездов на станции, находящиеся в текущий момент
-			-Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
-			-Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+				Класс Station (Станция):
+				Имеет название, которое указывается при ее создании
+				Может принимать поезда (по одному за раз)
+				Может возвращать список всех поездов на станции, находящиеся в текущий момент
+				Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
+				Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
 =end
-class Station
-	attr_reader :station_name, :stations, :trains 
+																						class Station
+																							
+																							attr_reader :station_name, :train_type
+																							#attr_writer 
 
-	def initialize(station_name)
-		@station_name = station_name
-		@trains = []
-	end
-	
-	def take_a_train(train)
-		@trains << train # или так - @trains.push = train
-	end
+																							def initialize(station_name)
+																								@station_name = station_name
+																								@trains = []
+																							end
+																							def add_train(train)
+																								@trains.push(train)
+																							end
+																							def show_all_trains_on_station
+																								@trains.each {|t| puts t}
+																							end
+																							def show_all_trains_by_type(train)
+																								@trains.each {|t| pust t.train_type} #__.find_all.....
+																							end
+																							def send_train(train)
+																								@trains.pop(train)
+																							end
 
-	def show_all_trains
-		@trains.each {|train| puts train }
-	end
-	
-	def show_trains_by_type
-		@trains.find_all {|train| puts train.type_of_train}
-	end
-	
-	def send_train(train)
-		@trains.delete(train)
-	end
-end
+																						end
+
 
 =begin
-Класс Route (Маршрут):
--Имеет начальную и конечную станцию, а также список промежуточных станций. 
+																										Класс Route 
+				Имеет начальную и конечную станцию, а также список промежуточных станций. 
 				Начальная и конечная станции указываютсся при создании маршрута, а промежуточные могут добавляться между ними.
-			-Может добавлять промежуточную станцию в список
-			-Может удалять промежуточную станцию из списка
-			-Может выводить список всех станций по-порядку от начальной до конечной
+				Может добавлять промежуточную станцию в список
+				Может удалять промежуточную станцию из списка
+				Может выводить список всех станций по-порядку от начальной до конечной
 =end
-class Route
-	attr_accessor :route 
-	def initialize(start_stantion, end_stantion)
-		@route = [start_stantion, end_stantion]
-	end
+																						class Route
 
-	def add_way_station_to_list(station)
-		@route.insert(-2, station)
-	end
+																							attr_reader :route
 
-	def del_way_station_from_list(station)
-		@route.delete(station)
-	end
-
-	def display_list_of_all_stations
-		@route.each {|station| puts station.station_name}
-	end
-end
+																							def initialize(start_station, end_station)
+																								@route = [start_station, end_station]
+																							end
+																							def add_way_station(station)
+																								@route.insert(-2, station)
+																							end
+																							def del_way_station(station)
+																								@route.delete_at(-2)
+																							end
+																							def show_all_stations
+																								@route.each {|stat| puts stat}
+																							end
+																						end
 
 =begin
-Класс Train (Поезд):
--Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, 
-	эти данные указываются при создании экземпляра класса
-			-Может набирать скорость
-			-Может возвращать текущую скорость
-			-Может тормозить (сбрасывать скорость до нуля)
-			-Может возвращать количество вагонов
-			-Может прицеплять/отцеплять вагоны (по одному вагону за операцию, 
-				метод просто увеличивает или уменьшает количество вагонов). 
-				Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
--Может принимать маршрут следования (объект класса Route). 
--При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
--Может перемещаться между станциями, указанными в маршруте. 
-	Перемещение возможно вперед и назад, но только на 1 станцию за раз.
--Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
+				Класс Train (Поезд):
+				Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, 
+																				эти данные указываются при создании экземпляра класса
+				Может набирать скорость
+				Может возвращать текущую скорость
+				Может тормозить (сбрасывать скорость до нуля)
+				Может возвращать количество вагонов
+				Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает 
+								или уменьшает количество вагонов). Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
+				Может принимать маршрут следования (объект класса Route). 
+			При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+	Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
+	Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
 =end
 class Train
 
-	attr_accessor :wagons, :speed, :train_number, :type_of_train, 
-
-	def initialize(train_number, type_of_train, wagons)
+	attr_reader :speed, :train_number, :train_type, :wagons_quantity, :current_route, :current_station, :pass
+	attr_writer :pass
+	def initialize(train_number, train_type, wagons_quantity)
 		@train_number = train_number
-		@type_of_train = type_of_train
-		@wagons = 0
+		@train_type = train_type
+		@wagons_quantity = wagons_quantity
+		@wagons_quantity = 0
 		@speed = 0
 	end
+
 	def speed_up
 		@speed += 10
 	end
-
-	def current_speed
-		@speed 
+	def show_speed
+		@speed
 	end
 
-	def stop_speed_to_zero
+	def speed_stop
 		@speed = 0
 	end
 
-	def display_num_of_wagons
-		@wagons
+	def show_quantity_of_wagons
+		@wagons_quantity
 	end
 
 	def add_wagon
 		if @speed == 0
-			@wagons += 1
-		else
-			puts "поезд в пути- не пирстегнуть вагон!"
-		end
-	end
-	def del_wagon
-		if @speed == 0 && @wagons >= 1
-			@wagons -= 1
-		else
-			puts "поезд  в пути - не отцепить ! или нет вагона.."
+			@wagons_quantity += 1
+		else 
+			puts "TRAIN IS GOING"
 		end	
 	end
-
-	def marshrut(route)
-		
-		#Может принимать маршрут следования (объект класса Route)
-		# что это значит ? 
+	def del_wagon
+		if @wagons_quantity >= 1 && @speed == 0 
+			@wagons_quantity -= 1 
+		end
 	end
 
-	def current_station
-#При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+	def current_route(route) # Может принимать маршрут следования (объект класса Route)
+		@current_route = route
+		#При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+		@current_station = @current_route.first
 	end
 
-	def move_to_station
-		#Может перемещаться между станциями, указанными в маршруте.
+	def go_ahead #Может перемещаться между станциями, указанными в маршруте. вперед  но только на 1 станцию за раз
+		if @current_station != @current_route.last # == @end_station
+			@current_station.route.add_way_station
+			
+
+		end
+	end
+	def go_back #Может перемещаться между станциями, указанными в маршруте. назад, но только на 1 станцию за раз
+	
 	end
 
-	def move_to_previous_station
-		#еремещение возможно вперед и назад, но только на 1 станцию за раз.
+	def show_stantions  # Возвращать предыдущую станцию, текущую, следующую, на основе маршрута	
 	end
-	def show_lcn_stations #LAST CURRTEN NEXT
-		#Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
-	end
-
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lssn3/t.rb
+++ lssn3/t.rb
@@ -1,11 +1,25 @@
-require_relative "1"
+require_relative "2"
 
-vokzal = Station.new("Airport")
-route1 = Route.new("Msc", "Vln")
-train1 = Train.new(111, "passanger", 6)
+#aaa = Station.new(Leningradsky_vokzal)
 
-vokzal.take_a_train(train1)
+#aaa.add_train
+		#show_all_trains_on_station
+		#show_all_trains_by_type
 
-p vokzal
-p train1
-p route1
+ttt = Train.new(666, "pass", 6)
+
+puts ttt.speed
+ttt.speed_up
+puts ttt.speed
+ttt.speed_stop
+puts ttt.speed
+ttt.speed_up
+ttt.speed_up
+ttt.speed_up
+puts ttt.speed
+ttt.speed_stop
+puts ttt.speed
+puts ttt.show_quantity_of_wagons
+ttt.speed_up
+ttt.add_wagon
+puts ttt.show_quantity_of_wagons
