d02c503affe4955cfe1be794f1d13fb0d6d3534e
Erikable <surmiloev@gmail.com>
2017-07-26 10:12:21 +0300

    lesson3done

--- zzz/trains/cargo_train.rb
+++
@@ -1,5 +0,0 @@
-class CargoTrain < Train
-	def initialize(number)
-		super(numder, :cargo)
-	end
-end
--- zzz/trains/passanger_train.rb
+++
@@ -1,5 +0,0 @@
-class PassangerTrain < Train
-	def initialize(number)
-		super(number, :passanger)
-	end
-end
