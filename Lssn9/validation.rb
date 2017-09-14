35ce53a0d1173300321d56f58f06caaae28c1586
Erikable <surmiloev@gmail.com>
2017-09-15 02:23:41 +0300

    valid

--- Lssn9/validation.rb
+++ Lssn9/validation.rb
@@ -1,4 +1,26 @@
 module Validation
 
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def validate(name, type, *arg)
+
+    end
+  end
+
+  module InstanceMethods
+    def valid?
+      validate!
+      raise
+      false
+    end
+
+    def validate!
+
+    end
+  end
 
 end
