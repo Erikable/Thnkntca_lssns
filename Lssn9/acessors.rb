06536be503e531d4aa4151903b57f6a727420a30
Erikable <surmiloev@gmail.com>
2017-09-15 02:53:33 +0300

    sm chngs

--- Lssn9/acessors.rb
+++ Lssn9/acessors.rb
@@ -4,10 +4,12 @@ module Acessors
     attributes.each do |attribute|
       var_attribute = "@#{attribute}".to_sym
       define_method(attribute) { instance_variable_get(var_attribute) }
-      define_method("#{attribute}=".to_sym) { |value| instance_variable_set(var_attribute, value) }
+      define_method("#{attribute}=".to_sym) do |value| 
+        instance_variable_set(var_attribute, value) 
       @history ||= {}
       @history[attribute] ||= []
       @history[attribute] << value #{speed =>[60]}
+    end
       define_method("#{attribute}_history") {@history[attribute]}
     end
   end
