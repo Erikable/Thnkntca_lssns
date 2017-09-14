b5055f2ca56e24a190eac859568c670ccf134100
Erikable <surmiloev@gmail.com>
2017-09-15 01:22:03 +0300

    strong_attr_acessor

--- Lssn9/acessors.rb
+++ Lssn9/acessors.rb
@@ -8,8 +8,11 @@ module Acessors
     end
   end
 
-  def self.strong_attr_acessor
-    
+  def self.strong_attr_acessor(name, class)
+    var_name = "@#{name}".to_sym
+    define_method(name) { instance_variable_get(var_name) }
+    define_method("#{name}=".to_sym) do |x|
+    raise TypeError, "Ошибка" unless x.class.is_a?(type)
+    instance_variable_set(var_name, x)
   end
-
 end
