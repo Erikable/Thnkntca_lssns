b9118123788f47c4dca4f83cbadadf8ed2645e96
Erikable <surmiloev@gmail.com>
2017-09-15 17:44:29 +0300

    validat

--- Lssn9/validation.rb
+++ Lssn9/validation.rb
@@ -7,20 +7,34 @@ module Validation
 
   module ClassMethods
     def validate(name, type, *arg)
-
+      @validations |= []
+      @validations << {name: name, type: type, args: args}
     end
   end
 
   module InstanceMethods
     def valid?
       validate!
-      raise
+      rescue
       false
     end
 
     def validate!
-
+      self.class.validate
     end
+
+    def type(name, attr, class)
+      raise
+    end
+
+    def presence(name, attr)
+      raise
+    end
+
+    def format(name, attr, format)
+      raise
+    end
+
   end
 
 end
